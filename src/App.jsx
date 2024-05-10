74c2329ec952758801328cfe154d5e8a5e7888b2
aliwdg16 <alialtoto@hotmail.com>
2024-05-10 08:50:14 +0200

    newcard newlogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Content from "./components/Content";
+import { ContentCard } from "./components/ContentCard";
+
+
 
 function App() {
   return (
     <>
       <Header />
-      <div className="">
-        <Content />
-      </div>
+      
+      <ContentCard />
+     
+
+
       <Footer />
+
+
     </>
   );
 }
