a378a3e1d1343dfbc4d8db7caf65a19e26c3bfed
aliwdg16 <alialtoto@hotmail.com>
2024-05-13 18:27:25 +0200

    singelbolg

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ContentCard } from "./components/ContentCard";
-
-
+import THÜRINGERHAUS from "./Fotosmitbeschreibung/THÜRINGERHAUS";
+import {BrowserRouter as Router, createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Singleblog from './components/Singleblog.jsx';
 
 function App() {
   return (
     <>
       <Header />   
       <ContentCard />
+      <THÜRINGERHAUS />
       <Footer />
-
-
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
+import {BrowserRouter as Router, createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Singleblog from './components/Singleblog.jsx';
+import { ContentCard } from './components/ContentCard.jsx';
+
+const router =createBrowserRouter([
+  {  path:'/' ,
+  element:<App />,
+},
+{  path:'blog' ,
+element:<ContentCard />,
+},
+{  path:'/singleblog' ,
+element:<Singleblog />,
+},
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
