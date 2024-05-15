43c9ad44ec100422ccc6abd17947532759280841
sidhdhali <sidhnakrani1911@gmail.com>
2024-05-15 15:48:37 +0200

    first commit ohne stylen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import "tw-elements-react/dist/css/tw-elements-react.min.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { ContentCard } from "./components/ContentCard";
-import THÜRINGERHAUS from "./Fotosmitbeschreibung/THÜRINGERHAUS";
-import {BrowserRouter as Router, createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import 'tw-elements-react/dist/css/tw-elements-react.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { ContentCard } from './components/ContentCard';
+import THÜRINGERHAUS from './Fotosmitbeschreibung/THÜRINGERHAUS';
+import { Routes, Route } from 'react-router-dom';
 import Singleblog from './components/Singleblog.jsx';
 
 function App() {
   return (
     <>
-      <Header />   
-      <ContentCard />
-      <THÜRINGERHAUS />
+      <Header />
+      {/* <THÜRINGERHAUS /> */}
+      <Routes>
+        <Route path='/' element={<ContentCard />} />
+        <Route path='/places/:id' element={<Singleblog />} />
+      </Routes>
       <Footer />
     </>
   );
